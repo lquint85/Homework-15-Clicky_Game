84f5e1db0c2a35d1132954bab04e7617df834491
Lucent <lquint85@gmail.com>
2019-02-25 00:36:49 -0600

    modified

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ function Header(props) {
             <div className="title">Clicky-Game</div>
             <div className="message">{props.message}</div>
             <div className="score">Score {props.score} of {props.total}</div>
-            <div className="topscore">TopScore {props.topscore} of {props.total}</div>
+            <div className="topscore">TopScore {props.topscore}  {props.score}</div>
             </header>
             <br/>
         </>
